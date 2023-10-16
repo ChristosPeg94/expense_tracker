4dabf689a5caf4dfaa5c80cd1406e0a2fc3b8dbf
Christos Pengaras <c.pengaras@gmail.com>
2023-10-16 22:08:52 +0300

    first commit

---
+++ src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,26 @@
+import './ExpensesFilter.css';
+
+const ExpensesFilter = (props) => {
+
+  // The value that is used in the component is passed on to the parent component
+  // throw props and is received from the parent component.
+  const dropdownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
+  };
+
+  return (
+    <div className='expenses-filter'>
+      <div className='expenses-filter__control'>
+        <label>Filter by year</label>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
+          <option value='2022'>2022</option>
+          <option value='2021'>2021</option>
+          <option value='2020'>2020</option>
+          <option value='2019'>2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
